0089435c1c8036f8c1ef03351350a2a7fd400452
semenyutenko <semenyutenko@gmail.com>
2023-09-13 02:11:20 +0300

    task_2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 fun main(args: Array<String>) {
-    //task_1
+    // task_1
     var x: Any? = null
     x = 4
     var y = x / 2.5 // Double
+
+    // task_2
+    val a = "hello"
+    val b = "aplana"
+    println("$a $b")
 }
